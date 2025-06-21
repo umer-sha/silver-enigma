b008e8e2e5c21415e918f81ac08ffbc1285af368
umer shah <umershahlatifi3@gmail.com>
2025-06-21 05:08:26 -0500

    Add files via upload

---
+++ Stopwatch.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function Stopwatch() {
+    const [isRunning, setIsRunning] = useState(false);
+    const [elapsedTime, setElapsedTime] = useState(0);
+    const intervalIdRef = useRef(null);
+    const startTimeRef = useRef(0);
+
+    useEffect(() => {
+        if (isRunning) {
+            intervalIdRef.current = setInterval(() => {
+                setElapsedTime(Date.now() - startTimeRef.current);
+            }, 10);
+        }
+
+        return () => {
+            clearInterval(intervalIdRef.current);
+        };
+    }, [isRunning]);
+
+    function start() {
+        setIsRunning(true);
+        startTimeRef.current = Date.now() - elapsedTime;
+    }
+
+    function stop() {
+        setIsRunning(false);
+    }
+
+    function reset() {
+        setIsRunning(false);
+        setElapsedTime(0);
+        startTimeRef.current = 0;
+    }
+
+    function formatTime() {
+        let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+        let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
+        let seconds = Math.floor((elapsedTime / 1000) % 60);
+        let milliseconds = Math.floor((elapsedTime % 1000) / 10);
+
+        return `${hours.toString().padStart(2, '0')}:${minutes
+            .toString()
+            .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${milliseconds
+            .toString()
+            .padStart(2, '0')}`;
+    }
+
+    return (
+        <div className='stopwatch'>
+            <div className='display'>{formatTime()}</div>
+            <div className='controls'>
+                <button className="start-button" onClick={start}>Start</button>
+                <button className="stop-button" onClick={stop}>Stop</button>
+                <button className="reset-button" onClick={reset}>Reset</button>
+            </div>
+        </div>
+    );
+}
+
+export default Stopwatch;
